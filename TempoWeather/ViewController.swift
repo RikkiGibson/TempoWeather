5bcf904380bac03127d3cc72c1d6f154e286ff8b
Rikki Gibson <rikkigibson@gmail.com>
2017-05-07 17:03:59 -0700

    Add a Carthage dependency and modify pbxproj

--- TempoWeather/ViewController.swift
+++ TempoWeather/ViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import PromiseKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        _ = Promise(value: "Hello, world")
+            .then(execute: { (s: String) -> Int in
+                print(s)
+                return 42
+            })
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
